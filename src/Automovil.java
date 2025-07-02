9edf984ddbdc4076b4bc7df5f8a2cd4d78d00455
Elvis Almonte <elvisalmonte14@gmail.com>
2025-07-01 23:23:39 -0400

    Creacion clase automovil

--- src/Automovil.java
+++ src/Automovil.java
@@ -1,2 +1,9 @@
 public class Automovil {
+    String fabricante;
+    String modelo;
+    String color;
+    float cilindrada;
+
+
+
 }
---
+++ src/EjemploAutomovil.java
@@ -0,0 +1,10 @@
+public class EjemploAutomovil {
+    public static void main(String[] args) {
+        Automovil subaru = new Automovil();
+
+        System.out.println("Fabricante: " + subaru.fabricante);
+        System.out.println("Modelo: " + subaru.modelo);
+        System.out.println("Color: " + subaru.color);
+        System.out.println("Cilindrada: " + subaru.cilindrada);
+    }
+}
